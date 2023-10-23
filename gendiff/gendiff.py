8c300b548f405396ee51c5499c3a3a48fb54e477
Tim Volkov <volkov.timm@gmail.com>
2023-10-23 16:38:34 +0500

    update bool format

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -28,8 +28,7 @@ def format_value(key, node):
 
     if key in node:
         return str(node[key]).lower() \
-            if node[key] in [True, False, None, 'null'] \
-            and isinstance(node[key], int) \
+            if node[key] is True or node[key] is False \
             else node[key]
 
 
